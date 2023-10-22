9659a7ff0551ee8fe716a058c0c9016407249f98
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-10-22 15:12:32 +0200

    👷  Improve addition by just dropping into exercices

--- create-dist.sh
+++ create-dist.sh
@@ -4,8 +4,12 @@
 mkdir ./dist
 cp index.html ./dist/index.html
 
+# use nullglob in case there are no matching files
+shopt -s nullglob
+
 # Define an array of strings
-repositories=("04-local-server-final" "05-transform-objects-final" "06-animations-final" "07-cameras-final" "08-fullscreen-and-resizing-final" "09-geometries-final" "10-debug-ui-final" "11-textures-final" "12-materials-final" "13-3d-text-final" "14-go-live-final" "15-lights-final" "16-shadows-final" "17-haunted-house-final" "18-particles-final" "19-galaxy-generator-final" "20-scroll-based-animation-final" "21-physics-final" "22-imported-models-final" "23-raycaster-and-mouse-events-final" "24-custom-models-with-blender-final" "25-environment-map-final" "26-realistic-render-final" "27-code-structuring-for-bigger-projects-final" "28-shaders-final")
+# repositories=("04-local-server-final" "05-transform-objects-final" "06-animations-final" "07-cameras-final" "08-fullscreen-and-resizing-final" "09-geometries-final" "10-debug-ui-final" "11-textures-final" "12-materials-final" "13-3d-text-final" "14-go-live-final" "15-lights-final" "16-shadows-final" "17-haunted-house-final" "18-particles-final" "19-galaxy-generator-final" "20-scroll-based-animation-final" "21-physics-final" "22-imported-models-final" "23-raycaster-and-mouse-events-final" "24-custom-models-with-blender-final" "25-environment-map-final" "26-realistic-render-final" "27-code-structuring-for-bigger-projects-final" "28-shaders-final")
+repositories=(./exercices/*)
 
 # Iterate over the elements in the array
 for repository in "${repositories[@]}"; do
---
+++ exercices/29-shader-patterns/src/shaders/test/vertex.glsl
@@ -0,0 +1,7 @@
+varying vec 2 vUv;
+
+void main()
+{
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vUv = uv;
+}
