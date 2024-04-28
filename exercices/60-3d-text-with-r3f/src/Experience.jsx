278449381bd6a0e9cb59c7941e38384e43a45f8a
laurentDellaNegra <laurent.dellanegra@gmail.com>
2024-04-28 14:42:47 +0200

    finish 60

--- exercices/60-3d-text-with-r3f/src/Experience.jsx
+++ exercices/60-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,84 @@
-import { OrbitControls } from '@react-three/drei'
+import {
+    useMatcapTexture,
+    Center,
+    Text3D,
+    OrbitControls,
+} from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
+import { useEffect, useRef } from 'react'
+// import { useState } from 'react'
+import * as THREE from 'three'
 
-export default function Experience()
-{
-    return <>
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
-        <Perf position="top-left" />
+export default function Experience() {
+    const [matcapTexture] = useMatcapTexture('8B892C_D4E856_475E2D_47360A', 256)
 
-        <OrbitControls makeDefault />
+    const refGroup = useRef([])
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+    useFrame((state, delta) => {
+        for (const donut of refGroup.current) {
+            donut.rotation.y += delta * 0.5
+        }
+    })
 
-    </>
+    useEffect(() => {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    return (
+        <>
+            <Perf position="top-left" />
+
+            <OrbitControls makeDefault />
+
+            {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+            <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
+
+            <Center>
+                <Text3D
+                    size={0.75}
+                    height={0.2}
+                    curveSegments={12}
+                    bevelEnabled
+                    bevelThickness={0.02}
+                    bevelSize={0.02}
+                    bevelOffset={0}
+                    bevelSegments={5}
+                    font={'./fonts/helvetiker_regular.typeface.json'}
+                    material={material}
+                >
+                    Hello R3F
+                </Text3D>
+
+                {/* <group ref={refGroup}> */}
+                {[...Array(100)].map((_value, i) => (
+                    <mesh
+                        key={i}
+                        ref={(element) => (refGroup.current[i] = element)}
+                        position={[
+                            (Math.random() - 0.5) * 10,
+                            (Math.random() - 0.5) * 10,
+                            (Math.random() - 0.5) * 10,
+                        ]}
+                        scale={0.2 + Math.random() * 0.2}
+                        rotation={[
+                            Math.random() * Math.PI,
+                            Math.random() * Math.PI,
+                            0,
+                        ]}
+                        geometry={torusGeometry}
+                        material={material}
+                    />
+                ))}
+                {/* </group> */}
+            </Center>
+        </>
+    )
 }
--- exercices/60-3d-text-with-r3f/src/index.jsx
+++ exercices/60-3d-text-with-r3f/src/index.jsx
@@ -7,12 +7,12 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        camera={ {
+        camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ 4, - 2, 6 ]
-        } }
+            position: [4, -2, 6],
+        }}
     >
         <Experience />
     </Canvas>
