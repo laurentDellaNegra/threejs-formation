a0cb929ba69440b3c15045f75cb5e50a6386f2e2
laurentDellaNegra <laurent.dellanegra@gmail.com>
2024-05-28 22:25:43 +0200

    finish 62

--- exercices/62-mouse-events-with-r3f/src/Experience.jsx
+++ exercices/62-mouse-events-with-r3f/src/Experience.jsx
@@ -1,37 +1,70 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, meshBounds } from '@react-three/drei'
 import { useRef } from 'react'
+import * as THREE from 'three'
 
-export default function Experience()
-{
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    const hamburger = useGLTF('hamburger.glb')
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
-    return <>
+    const eventHandler = (e) => {
+        // console.log('event', e)
+        // e.object.material.color.set(
+        //     `hsl(${Math.random() * 360}, 100%, 75%)`
+        // )
+        cube.current.material.color.set(
+            `hsl(${Math.random() * 360}, 100%, 75%)`
+        )
+    }
 
-        <OrbitControls makeDefault />
+    return (
+        <>
+            <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+            <directionalLight position={[1, 2, 3]} intensity={4.5} />
+            <ambientLight intensity={1.5} />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <mesh position-x={-2} onClick={(e) => e.stopPropagation()}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <mesh
+                ref={cube}
+                position-x={2}
+                scale={1.5}
+                raycast={meshBounds}
+                onClick={eventHandler}
+                onPointerEnter={() => {
+                    document.body.style.cursor = 'pointer'
+                }}
+                onPointerLeave={() => {
+                    document.body.style.cursor = 'default'
+                }}
+            >
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
 
-    </>
+            <primitive
+                object={hamburger.scene}
+                scale={0.25}
+                position={1}
+                onClick={(e) => {
+                    console.log(e)
+                    e.stopPropagation()
+                }}
+            />
+        </>
+    )
 }
